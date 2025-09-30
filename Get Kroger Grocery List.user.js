41bb58ffd75382a42a64cd0ff07d2bbdfe74eba5
ThermoMan <thestalwart1-tstat@yahoo.com>
2025-09-30 16:13:46 -0500

    Update Get Kroger Grocery List.user.js
    Renamed (internally) script
    Total rewrite.

--- Get Kroger Grocery List.user.js
+++ Get Kroger Grocery List.user.js
@@ -1,163 +1,317 @@
 // ==UserScript==
-// @name         Get Kroger Grocery List
+// @name         Get Grocery Shopping History
 // @namespace    PrettyDarnUseful
-// @version      0.2
-// @description  Get Kroger Grocery List
+// @version      0.6
+// @description  Get Grocery Shopping History
 // @author       ThermoMan
 // @match        https://www.(dillons|kroger).com/mypurchases/detail/*
 // @run-at       document-end
-// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js
 // ==/UserScript==
 
-"use strict";
-var debug = false;
+/**
+ ** TO DO LIST
+ **
+ ** File Format
+ **   Instead of building a giant text field build a JSON object
+ **     Add a checkbox to save as CSV or JSON?
+ **   Excel is not recognizing the first column in the CSV file as a date, it thinks that it is text.
+ **
+ ** Data Source
+ **   Test delivery and get that to work.
+ **   Either duplicate this script for Walmart or add a detection mode.  Filename and a data column should also indicate the difference
+ **
+ ** Parsed Data
+ **   Perhaps add the mode to the CSV file?  'in store', 'pickup', 'delivery', 'fuel station'
+ **   Add store name in case someone shops at both Kroger and Dillons.
+ **   Perhaps add store address (or store #) to differentiate in case a product is only at one store, but not the other.
+ **
+ ** Inner Smarts
+ **   Should I do something when the price shown in the QUANTITY column does not match the EXTENDED PRICE (for instance note the discount or the tax?)
+ **     Fuel price actually has a message about discount in hidden text.
+ **   Move the character translate into a list and just iterate through the list
+ **
+ ** Test Cases before commit
+ **   Normal in store purchase
+ **   Online order with pickup
+ **   Fuel Station
+ **/
 
-var start = function( jQuery ){
-  addButton( "Get List" );
-};
+/**
+ ** My reference notes
+ **
+ ** Fix Firefox warning of slow scripts https://support.mozilla.org/en-US/kb/warning-unresponsive-script
+ **
+ ** GreaseMonkey wiki https://wiki.greasespot.net/Main_Page
+ ** GreaseMonkey docs http://greasemonkey.win-start.de/
+ **
+ **/
 
+(function(){
+  'use strict';
+  var debug = false;
 
-function buttonClickAction(){
-  log( "clear" );
-  log( "Trying to fetch the grocery shopping list." );
-
-  // So the whitespace separates the "siblings" in text.
-  var tripDate = $( "span:contains('Order date:')" ).find( "br" ).get(0).nextSibling.nextSibling.nodeValue;
-
-  log( "Date of purchase was: " + tripDate );
-
-  var ii = 0;
-  var saveString = "date, name, size, price, qty, ext_price\n";
-  $( "div[class='PH-ProductCard-container w-full p-16']" ).each( function(){
-    ii++;
-    var product;
-    var productName;
-    var productSize;
-    var extendedPrice;
-    var quantity;
-    var unitPrice;
-
-    var message = $(this)[0].childNodes[0].childNodes[0].innerText;
-    if( message == "Out-of-Stock" ){
-			product = $(this)[0].childNodes[1].childNodes[1].childNodes[0].childNodes[0];
-      extendedPrice = 0;
-      quantity = 0;
-      unitPrice = 0;
-    }
-    else if( message == "Substitution" ){
-			product = $(this)[0].childNodes[1].childNodes[1].childNodes[0].childNodes[0];
-    }
-    else {
-      product = $(this)[0].childNodes[0].childNodes[1].childNodes[0].childNodes[0];
-    	message = "Purchase";
-    }
-
-    productName = product.childNodes[0].childNodes[0].innerText;
-    log( "Product "+ii+" name was: " + productName );
-
-    productSize = product.childNodes[1].innerText;
-    log( "Product "+ii+" size was: " + productSize );
-// Later on separate units  "25.4 oz"   "30 fl oz"  "2.5 gal"   "6 ct / 3 oz"    "1 lb"
-
-    if( message == "Substitution" || message == "Purchase" ){
-	    extendedPrice = $(this).find( "div[class='PH-ProductCard-Total flex flex-col items-start mb-8 lg:w-1/3']" )[0].firstChild.value;
-	    quantity = $(this).find( "span[class='kds-Text--s text-default-500 body-xs']" ).text();
-      unitPrice = $(this).find( "span[class='kds-Text--s text-default-500 body-xs']" ).find( "span" ).text();
-    }
-	  log( "Product "+ii+" extended price was: " + extendedPrice );
-	  log( "Product "+ii+" quantity was: " + quantity );
-	  log( "Product "+ii+" unit price was: " + unitPrice );
-
-    saveString = saveString + tripDate + "," + productName + "," + productSize + "," + unitPrice + "," + quantity + "," + extendedPrice  + "\n";
-  });
-  save( saveString, tripDate );
-}
-
-
-// This function injects CSS into the page
-// Need to adapt this function to use jQuery
-function addGlobalStyle( css ){
-  var head;
-  var style;
-  head = document.getElementsByTagName( "head" )[0];
-  // if( !head ){ return; }
-  style = document.createElement( "style" );
-  style.type = "text/css";
-  style.innerHTML = css;
-  head.appendChild( style );
-}
-
-
-// This function adds a button to the screen to trigger the script
-// Need to adapt this function to use jQuery
-function addButton( text ){
-  // Add style
-  var cssStr = "#rs_myContainer{ position: fixed; top: 121px; left: 7px; font-size: 20px; background-color: darkgray; border: 3px outset black; margin: 5px; opacity: 0.9; z-index: 1100; padding: 5px 20px; } #rs_myButton{ cursor: pointer; background-color: darkgray; }";
-  addGlobalStyle( cssStr );
-
-  // Add button
-  var zNode = document.createElement( "div" );
-  zNode.innerHTML = "<button id='rs_myButton' type='button'>"+text+"</button>";
-  zNode.setAttribute( "id", "rs_myContainer" );
-  document.body.appendChild( zNode );
-
-  // Activate the newly added button.
-  document.getElementById( "rs_myButton" ).addEventListener( "click", buttonClickAction, false );
-}
-
-
-// This function only writes to the console if debug is true.
-function log( pMessage ){
-  if( debug ){
-    if( pMessage == "clear" ){
-      console.clear();
-    }
-    else {
-    	console.debug( pMessage );
+  // Log function for debugging
+  function log( pMessage ){
+    if( debug ){
+      if( pMessage === 'clear' ){
+        console.clear();
+      }
+      else{
+        console.debug( pMessage );
+      }
     }
   }
-}
 
-// This function always writes to the console and saves the data as a CSV file.
-function save( pMessage, pDate ){
-  console.log( pMessage );
-  var dateParts = pDate.split( "/" );	// Split the date up and rearrange it into a logical order while removing illegal file system chracters
-  var sDate = dateParts[2] + "-" + dateParts[0].padStart( 2, "0" ) + "-" + dateParts[1].padStart( 2, "0" );
-  
-  pMessage = pMessage.replaceAll( "®", "" );	// Remove dumb mark
-	pMessage = pMessage.replaceAll( "’", "'" );	// Fix stupid single quotes
 
-  var anchor = document.querySelector( "a" );
-  anchor.onclick = function(){
-    anchor.href = "data:text/plain;charset=utf-8," + encodeURIComponent( pMessage );
-    anchor.download = "Grocery Export "+sDate+".csv";
+  function buttonClickAction(){
+    log( 'buttonClickAction started' );
+
+    // Show spinning notifier
+    $( '#rs_grocery_button' ).prop( 'disabled', true ).addClass( 'disabled' );
+    $( '#rs_grocery_button .glyph-spinner' ).css( 'visibility', 'visible' ).addClass( 'spinning' );
+
+    try {
+      // Get trip date from URL (note that tripDate is a STRING not a Date object)
+      var tripDate = window.location.href.split('~')[2];
+      log( 'Date of purchase was: ' + tripDate );
+      var storeName = window.location.href.split('.')[1];
+
+      var product;
+      var productName;
+      var productData1;
+      var productData2;
+      var productSize;
+      var productUnits;
+      var productCount;
+      var extendedPrice;
+      var quantity;
+      var unitPrice;
+      var message;
+      var ii = 0;
+      var saveString = "STORE,DATE,MESSAGE,NAME,SIZE,UNITS,COUNT,UNIT_PRICE,QTY,EXT_PRICE\n";
+      $( 'div[class="PH-ProductCard-container w-full p-16"]' ).each( function(){
+//		    log( 'buttonClickAction looping: ' + ii );
+        product = '';
+        productName = '';
+        productData1 = '';
+        productData2 = '';
+        productSize = '';
+        productUnits = '';
+        productCount = '';
+        extendedPrice = '';
+        quantity = '';
+        unitPrice = '';
+        var message = $( this )[0].childNodes[0].childNodes[0].innerText;
+        if( 'Out-of-Stock' === message ){
+          product = $( this )[0].childNodes[1].childNodes[1].childNodes[0].childNodes[0];
+          productName = product.childNodes[0].childNodes[0].innerText;
+//log( 'productName (' + productName + ')' )
+          productData1 = '';
+          productData2 = '';
+          extendedPrice = 0;
+          quantity = 0;
+          unitPrice = 0;
+        }
+        else if( 'Substitution' === message  ){
+debugger;
+//          product = $( this )[0].childNodes[1].childNodes[1].childNodes[0].childNodes[0];
+          product = $( this )[0].childNodes[2].childNodes[0].childNodes[1].childNodes[0];
+//          productName = product.childNodes[0].childNodes[0].innerText;
+          productName = product.childNodes[0].childNodes[0].innerText;
+          productData1 = product.childNodes[1].innerText;
+          productData2 = $( this ).find( 'span[class="kds-Text--s text-default-500 body-xs"]' ).text();
+        }
+        else if( 'FUEL' === message  ){
+debugger;
+          product = $( this )[0].childNodes[0].childNodes[1].childNodes[0];
+          productName = product.childNodes[0].childNodes[0].innerText;
+          productData1 = '';
+          productData2 = product.childNodes[1].childNodes[1].innerText;
+        }
+        else{
+          message = 'Purchase';
+          product = $( this )[0].childNodes[2].childNodes[0].childNodes[1].childNodes[0];
+
+//        productName is something like "Ben's Original Enriched Long Grain White Rice, Parboiled Rice, 5 lb Bag"
+          productName = product.childNodes[0].childNodes[0].innerText;
+
+
+//        productData1 is something like "5 lb" or "4 ct / 10.5 oz" or sometimes the evil "net wt 38 oz" or "bag / 3 lb"
+          if( 'SNAP EBT' === product.childNodes[0].childNodes[1].innerText ){
+	          productData1 = product.childNodes[0].childNodes[2].innerText;
+          }
+          else{
+	          productData1 = product.childNodes[0].childNodes[1].innerText;
+          }
+          productData1 = productData1.replace(/net wt /g,'').replace(/bag \/ /g,'');  // Remove evil leading text
+          log( 'productData1 ' + productData1 );
+
+          
+//        productData2 is something like "1 x $10.99/each" - under the large text decorated extended price
+          productData2 = product.childNodes[1].childNodes[1].innerText;
+        }
+        // Parse productData1 to get count, size, and units.
+        if( productData1.indexOf( '/' ) > -1 ){
+          var productParts = productData1.split( ' / ' );
+          var p1 = productParts[0];
+          var pCC = p1.split( ' ' );
+          productCount = pCC[0];
+          var p2 = productParts[1];
+          var pSU = p2.split( ' ' );
+          productSize = pSU[0];
+          productUnits = pSU[1];
+        }
+        else if( message !== 'FUEL' ){
+          var productParts = productData1.split( / (.+)/ );
+          productCount = 1;
+          productSize = productParts[0];
+          productUnits = productParts[1];
+        }
+
+        // Parse quantity
+        var productParts = productData2.split( ' x ' );
+        var p1 = productParts[0];
+        if( p1.indexOf( ' ' ) > -1 ){
+          var pSU = p1.split( ' ' );
+          productCount = 1;
+          productSize = pSU[0];
+          productUnits = pSU[1];
+          quantity = 1;
+        }
+        else{
+          quantity = p1;
+        }
+        if( 'FUEL' === message  ){
+          var p2 = productParts[1];
+          var pPU = p2.split( '/' );
+          unitPrice = pPU[0];
+          quantity = 1;
+        }
+        else{
+          unitPrice = productParts[1].split('/')[0];
+        }
+        extendedPrice = quantity * unitPrice.split('$')[1];
+
+        saveString = saveString + storeName + "," + tripDate + "," + message + "," + productName.replace(/,/g,'') + "," + productSize + "," + productUnits + "," + productCount + "," + unitPrice + "," + quantity + "," + extendedPrice + "\n";
+
+        ii++;
+      });
+      save( saveString, tripDate );
+    }
+    catch( error ){
+      log( 'Error in buttonClickAction: ' + error );
+      log( 'Last productName was ' + productName );
+      alert( 'Error message here - tell user there is no file ouput.' );
+    }
+    finally{
+      // Hide spinning notifier and re-enable button
+      $( '#rs_grocery_button .glyph-spinner' ).css( 'visibility', 'hidden' ).removeClass( 'spinning' );
+      $( '#rs_grocery_button' ).prop( 'disabled', false ).removeClass( 'disabled' );
+    }
   }
-  anchor.click();
-
-}
 
 
-// This function laods jQuery manually if for some reason the @require does not work.
-function add_jQuery( callbackFn, jqVersion ){
-  var jqVersion   = jqVersion || "1";
-  var D           = document;
-  var targ        = D.getElementsByTagName( "head" )[0] || D.body || D.documentElement;
-  var scriptNode  = D.createElement( "script" );
-  scriptNode.src  = "https://ajax.googleapis.com/ajax/libs/jquery/" + jqVersion + "/jquery.min.js";
-  scriptNode.addEventListener( "load", function(){
-    var innerNode          = D.createElement( "script" );
-    innerNode.textContent  = "var gm_jQuery  = jQuery.noConflict( true );\n" + "(" + callbackFn.toString() + ")( gm_jQuery );";
-    targ.appendChild( innerNode );
-  }, false );
-  targ.appendChild( scriptNode );
-}
+  function addToggleButton(){
+    log( 'addToggleButton started' );
+
+    var $button = $( '<button>', {
+      id: 'rs_grocery_button',
+      html: '<span class="glyph-spinner" style="margin-right: 10px; visibility: hidden; font-size: 16px; display: inline-block;">&#10043;</span> Get Grocery List',
+      css: {
+        position: 'fixed',
+        top: '200px',
+        right: '100px',
+        zIndex: 9999,
+        padding: '12px 20px',
+        background: 'linear-gradient(135deg, #4CAF50, #2E7D32)',
+        color: '#FFFFFF',
+        border: 'none',
+        cursor: 'pointer',
+        borderRadius: '8px',
+        boxShadow: '0 4px 8px rgba(0, 0, 0, 0.3)',
+        font: '13px "Open Sans", Arial, sans-serif',
+        fontWeight: '600',
+        textTransform: 'uppercase',
+        letterSpacing: '1.2px',
+        transition: 'all 0.2s ease'
+      }
+    }).appendTo( 'body' );
+
+    // Inject spinner CSS
+    var css = `
+      .glyph-spinner.spinning {
+        animation: spin 1s linear infinite;
+        display: inline-block;
+      }
+      @keyframes spin {
+        0% { transform: rotate(0deg); }
+        100% { transform: rotate(360deg); }
+      }
+      @-webkit-keyframes spin {
+        0% { -webkit-transform: rotate(0deg); }
+        100% { -webkit-transform: rotate(360deg); }
+      }
+      @-moz-keyframes spin {
+        0% { -moz-transform: rotate(0deg); }
+        100% { -moz-transform: rotate(360deg); }
+      }
+    `;
+    $( '<style>' ).text( css ).appendTo( 'head' );
+    log( 'Spinner CSS injected' );
+
+    $button.on( 'mouseenter', function(){
+      if( ! $button.hasClass( 'disabled' ) ){
+        $button.css({
+          boxShadow: '0 6px 12px rgba(0, 0, 0, 0.4), 0 0 10px rgba(76, 175, 80, 0.7)',
+          transform: 'translateY(-2px)'
+        });
+      }
+    });
+
+    $button.on( 'mouseleave', function(){
+      $button.css({
+        boxShadow: '0 4px 8px rgba(0, 0, 0, 0.3)',
+        transform: 'translateY(0)'
+      });
+    });
+
+    $button.on( 'click', function(){
+      if( ! $button.hasClass( 'disabled' ) ){
+        log( 'Button clicked' );
+        $button
+          .animate({ transform: 'scale(0.95)' }, 100 )
+          .animate({ transform: 'scale(1)' }, 100 );
+        buttonClickAction();
+      }
+    });
+
+    return $button[0];
+  }
 
 
-// Make sure that some version of jQuery is loaded before going on
-if( typeof jQuery === "function" ){
-  log( "Running with local copy of jQuery!" );
-  start( jQuery );
-}else{
-  log( "Fetching jQuery from some 3rd-party server." );
-  add_jQuery( start, "3.5.1" );
-}
+  function save( pMessage, pDate ){
+    log( save );
+
+    // Convert bad characters to normal ones or remove them entirely.
+    pMessage = pMessage.replaceAll( '®', '' );
+    pMessage = pMessage.replaceAll( '™', '' );
+    pMessage = pMessage.replaceAll( '’', "'" );
+
+    var anchor = document.createElement( 'a' );
+    anchor.href = 'data:text/plain;charset=utf-8,' + encodeURIComponent( pMessage );
+    anchor.download = "Grocery Export " + pDate + ".csv";		// Note that pDate is a STRING not a Date object.
+    document.body.appendChild( anchor );
+    anchor.click();
+    document.body.removeChild( anchor );
+  }
+
+
+  function triggerThings(){
+    log( 'triggerThings' );
+    const button = addToggleButton();
+	  addToggleButton();
+  }
+
+  window.setTimeout( triggerThings, 500 );
+})();
